3dde3df698a5a3094316b8999dea23e6a9caabc1
MancillaO <edgarmncllav04@gmail.com>
2025-06-13 11:45:28 -0600

    Extraer formulario para regisrar fuera del modal hacia la vista principal

--- resources/views/membresias/Nueva.blade.php
+++ resources/views/membresias/Nueva.blade.php
@@ -3,20 +3,57 @@
 @section('title', 'Nueva membresía')
 
 @section('content_header')
-    <div class="col-lg-12 text-right">
-        <div class="btn-group" role="group" aria-label="Acciones de Membresía">
-            <button class="btn btn-success mr-2" data-toggle='modal' data-target="#createMembresia">
-                <i class="fa fa-plus"></i> Registrar Membresía
-            </button>
-        </div>
-    </div>
+    <h1>Nueva Membresía</h1>
 @endsection
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="card card-success">
+        <div class="card-header">
+            <h3 class="card-title">Registrar Nueva Membresía</h3>
+        </div>
+        <form action="{{ route('membresias.store') }}" method="POST">
+            @csrf
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label for="id_usuario">Usuario(*)</label>
+                            <select class="form-control" id="id_usuario" name="id_usuario" required>
+                                <option value="" disabled selected>Seleccione un usuario</option>
+                                @foreach ($usuarios as $usuario)
+                                    <option value="{{ $usuario->id }}">{{ $usuario->name }} {{ $usuario->last_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
 
-    {{-- Aquí incluimos el modal para registrar membresías --}}
-    @include('membresias.create')
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="clases_adquiridas">Clases Adquiridas(*)</label>
+                            <input type="number" class="form-control" id="clases_adquiridas" name="clases_adquiridas" min="0" required>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="clases_disponibles">Clases Disponibles(*)</label>
+                            <input type="number" class="form-control" id="clases_disponibles" name="clases_disponibles" min="0" required>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="clases_ocupadas">Clases Ocupadas(*)</label>
+                            <input type="number" class="form-control" id="clases_ocupadas" name="clases_ocupadas" min="0" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-success float-right">Registrar Membresía</button>
+            </div>
+        </form>
+    </div>
 @endsection
 
 @section('css')
@@ -25,5 +62,5 @@
 @endsection
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    {{-- Scripts personalizados opcionales --}}
 @endsection
