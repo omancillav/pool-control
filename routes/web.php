3fe042ac538ec44565312428456477800f17a187
omancillav <edgarmncllav04@gmail.com>
2025-08-05 12:51:33 -0600

    feat: Implement CheckInactivity middleware to handle user session timeouts

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use App\Http\Controllers\ClaseController;
 use Illuminate\Support\Facades\Route;
@@ -15,59 +15,59 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('inactivity');
 
 // Rutas de Membresías
 Route::get('/membresias', [MembresiaController::class, 'index'])
     ->name('membresias.nueva')
-    ->middleware(['auth', 'rol:Administrador']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador']);
 Route::get('/membresias/lista', [MembresiaController::class, 'list'])
     ->name('membresias.list')
-    ->middleware(['auth', 'rol:Administrador,Cliente']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Cliente']);
 Route::post('/membresias/store', [MembresiaController::class, 'store'])
     ->name('membresias.store')
-    ->middleware(['auth', 'rol:Administrador']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador']);
 Route::put('/membresias/{membresia}', [MembresiaController::class, 'update'])
     ->name('membresias.update')
-    ->middleware(['auth', 'rol:Administrador']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador']);
 Route::delete('/membresias/{id}', [MembresiaController::class, 'destroy'])
     ->name('membresias.destroy')
-    ->middleware(['auth', 'rol:Administrador']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador']);
 
 // Rutas para Usuarios
 Route::get('/usuarios', [UserController::class, 'index'])
     ->name('usuarios.nueva')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::get('/usuarios/lista', [UserController::class, 'list'])
     ->name('usuarios.list')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::post('/usuarios/store', [UserController::class, 'store'])
     ->name('usuarios.store')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::put('/usuarios/{usuario}', [UserController::class, 'update'])
     ->name('usuarios.update')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::delete('/usuarios/{id}', [UserController::class, 'destroy'])
     ->name('usuarios.destroy')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 
 
 // Rutas de Clases
 Route::get('/clases', [ClaseController::class, 'index'])
     ->name('clases.nueva')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::get('/clases/lista', [ClaseController::class, 'list'])
     ->name('clases.list')
-    ->middleware(['auth', 'rol:Administrador,Cliente,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Cliente,Profesor']);
 Route::post('/clases/store', [ClaseController::class, 'store'])
     ->name('clases.store')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::put('/clases/{clase}', [ClaseController::class, 'update'])
     ->name('clases.update')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 Route::delete('/clases/{id}', [ClaseController::class, 'destroy'])
     ->name('clases.destroy')
-    ->middleware(['auth', 'rol:Administrador,Profesor']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador,Profesor']);
 
 // Ruta para el Aviso de Privacidad
 Route::get('/aviso-de-privacidad', function () {
@@ -86,4 +86,4 @@ Route::get('auth/facebook/callback', [FacebookAuthController::class, 'callback']
 // Ruta para ver los logs de actividad
 Route::get('/logs', [LogController::class, 'index'])
     ->name('logs.index')
-    ->middleware(['auth', 'rol:Administrador']);
+    ->middleware(['auth', 'inactivity', 'rol:Administrador']);
